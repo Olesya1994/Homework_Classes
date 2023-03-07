14416ed11692f4a3259802f6b6ef486b858040b8
Olesya1994 <olesia.liakhor@gmail.ru>
2023-03-07 17:12:49 +0300

    Homework methods of objects

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,15 @@ public class Main {
             Author author2 = new Author("Лев", "Толстой");
             Book book1 = new Book(author1, 1928, "Мастер и маргарита");
             Book book2 = new Book(author2, 1878, "Анна Каренина");
+            Book book3 = new Book(author2, 1878, "Анна Каренина");
             //System.out.println("book1.getName() = " + book1.getName());
             //System.out.println("book2.getName() = " + book2.getName());
             //System.out.println("author1.getName() = " + author1.getAuthorName());
             //System.out.println("author2.getLastName() = " + author2.getAuthorLastName());
-            book2.setYear(1872);
+            //book2.setYear(1872);
             System.out.println("book2.getYear() = " + book2.getYear());
-
-
+            System.out.println("book1 = " + book1);
+            System.out.println(book2.hashCode());
+            System.out.println("book2.equals(book3) = " + book2.equals(book3));
         }
 }
